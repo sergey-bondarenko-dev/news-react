d1ba47e59016a54dd00ca5d83fb6b6a0235eb974
TomBombadiI <78550692+TomBombadiI@users.noreply.github.com>
2026-04-28 15:47:48 +0700

    add dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import Header from "./components/Header/Header";
 import MainPage from "./pages/MainPage/MainPage";
+import { useTheme } from "./context/ThemeContext";
 
 function App() {
+  const { isDark } = useTheme();
+
   return (
-    <>
+    <div className="app" data-theme={isDark ? 'dark' : 'light'}>
       <Header />
       <div className="container">
         <MainPage />
       </div>
-    </>
+    </div>
   )
 }
 
