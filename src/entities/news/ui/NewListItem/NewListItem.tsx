c0f263a579104d4106cb19812ff9e0048b507b71
Sergey Bondarenko <sergey.bondarenko457@gmail.com>
2026-05-01 17:12:51 +0700

    add routing

--- src/entities/news/ui/NewListItem/NewListItem.tsx
+++ src/entities/news/ui/NewListItem/NewListItem.tsx
@@ -2,6 +2,8 @@ import { INews } from '@/shared/interfaces';
 import { formatTimeAgo } from '@/shared/date';
 import { Image } from '@/shared/ui';
 import styles from './NewListItem.module.css';
+import { Link } from 'react-router-dom';
+import { routes } from '@/shared/config';
 
 interface Props {
     item: INews,
@@ -18,7 +20,9 @@ const NewListItem = (props: Props) => {
             <Image src={item.image} className={styles.image} />
             <div className={styles.body}>
                 <h3 className={styles.title}>
-                    {item.title}
+                    <Link to={routes.newsDetails.to(item.id)}>
+                        {item.title}
+                    </Link>
                 </h3>
                 <p className={styles.extra}>
                     {formatTimeAgo(item.published)} by {item.author}
